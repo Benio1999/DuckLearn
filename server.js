87c9ad7bc11dc6d81620ab38c8062c89c0d2d186
Castiel-Tyler <castieltyleraiko@gmail.com>
2025-11-03 09:35:47 -0300

    atualização server (criação das const)

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
-const moongose = require('mongoose')
-const API_URL = 'http://localhost:3004/server.js';
 require('dotenv').config()
+
+const API_URL = 'http://localhost:3004/server.js';
+const moongose = require('mongoose');
+const cors = require ('cors');
+const express = require ('express');
+const jwt = require ('jsonwebtoken');
+
+const User = require ('User');
+const { default: mongoose } = require('mongoose');
+
+//conexão do mongodb
+
+mongoose.connect(mongoURI)  
+    .then(() => console.log('Conectado ao Mongo DB'))
+
+
+
+
