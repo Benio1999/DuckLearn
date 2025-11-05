d9b6cfd460d596249e19e9e40a15c2fde196ee51
AlguemCOnhecido <igorlimaopp@gmail.com>
2025-11-05 08:45:42 -0300

    coisas do louis

---
+++ pastaL/pasta nada importante/quiz4/script.js
@@ -0,0 +1,6 @@
+const btnCollapse = document.getElementById('btn-collapse');
+const sidebar = document.getElementById('sidebar');
+btnCollapse.addEventListener('click', () => {
+    sidebar.classList.toggle('collapsed');
+});
+/*  */
