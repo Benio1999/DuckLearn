35095ae63f9af7b1ee36decc635a11d967b50559
Castiel-Tyler <castieltyleraiko@gmail.com>
2025-11-05 11:30:40 -0300

    atualização de funções server

--- js/main/script.js
+++ js/main/script.js
@@ -1,2 +1,21 @@
-const API_URL = 'http://localhost:3004/server.js';
 
+const inscrever = document.querySelector('.btn-signup');
+const login = document.querySelector('.btn-login');
+const inscrever_se = document.querySelector('.btn-modal')
+const modal = document.querySelector('.modal')
+
+
+
+inscrever.addEventListener(('click'), function() {
+    window.location.href ='../registro/inscrição.html';
+});
+
+inscrever_se.addEventListener(('click'), () =>{
+    modal.style.display = 'flex';
+    
+})
+
+
+login.addEventListener(('click'), function() {
+    window.location.href ='../login/login.html'
+})
